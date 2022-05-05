ea5a35845a2c347ec9920c5d181f1733a4a3ebed
kritoma <toma.kristian97@gmail.com>
2022-05-05 15:12:28 +0200

    modifiche snack-2

--- snack-2/main.js
+++ snack-2/main.js
@@ -4,11 +4,10 @@ let nArray
 for (let i = 0; i < n; i++) {
     nArray = new Array();
     
-    if( nArray.length < 10){
-        for (let i = 0; i < 10; i++) {
-            nArray.push(Math.floor(Math.random() * 100) + 1);            
-        }
+    for (let i = 0; i < 10; i++) {
+        nArray.push(Math.floor(Math.random() * 100) + 1);            
     }
+
 
     console.log(nArray)
 }
